742227bc9135a2d37bf86c0e66eee6fc4c491616
Rudresh Mishra <rudreshmishrasde@gmail.com>
2021-08-04 12:56:26 +0530

    updated Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,34 @@
 import React , {useState , useEffect} from 'react';
 import './Nav.css';
 function Nav() {
-     const [show , handleShow] = useState(false);
-    useEffect(() =>{
-        window.addEventListener('scroll' , () =>{
-            if(Window.scrollY > 100){
-              handleShow(true);
-            }else handleShow(false);
-        });
-        return () =>{
-            window.removeEventListener('scroll');
-        };
-    }, 
-    []) ; 
+    const [show, handleShow] = useState(false);
+    useEffect(() => {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+          handleShow(true);
+        } else {
+          handleShow(false);
+        }
+      });
+      return () => {
+        window.removeEventListener("scroll");
+      };
+    }, []);
+  
     return (
-        <div className = {`navbar ${show && 'nav_black'}`}>
-            
-                <img className = "nav-logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt = "NetFlix logo"></img>
-            
-
-            
-                <img className = "nav-avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt = "avatar logo"></img>
-            
-            
-        </div>
-    )
-}
-
- export default Nav
+      <div className={`nav ${show && "nav_black"}`}>
+        <img
+          className="nav_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+          alt="Netflix Logo"
+        />
+        <img
+          className="nav_avatar"
+          src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+          alt="Avatar"
+        />
+      </div>
+    );
+  }
+  
+  export default Nav;
